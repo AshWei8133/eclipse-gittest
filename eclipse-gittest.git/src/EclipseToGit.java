3684ddb740f9322df90819f93eebc3e8d55f3b56
AshWei8133 <ashee8133@gmail.com>
2024-12-15 14:46:51 +0800

    Merge branch 'main' of https://github.com/AshWei8133/eclipse-gittest.git into main

--- eclipse-gittest.git/src/EclipseToGit.java
+++ eclipse-gittest.git/src/EclipseToGit.java
@@ -4,6 +4,8 @@ public class EclipseToGit {
 	public static void main(String[] args) {
 		System.out.println("HelloWord");
 		System.out.println("HeyWord");
+		System.out.println("shiba");
+		
 
 
 	}
